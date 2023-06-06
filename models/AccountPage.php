0756597812ca891a549ebb04afab70a70ec8a075
apademide <contact@apademide.ch>
2023-06-06 11:06:38 +0200

    files: init models

---
+++ models/AccountPage.php
@@ -0,0 +1,11 @@
+<?php
+
+use Kirby\Cms\Page;
+
+use Kirby\Database\Database;
+use Kirby\Database\Sql\Sqlite;
+
+// A single user's page
+class AccountPage extends Page
+{
+}
---
+++ models/AccountstPage.php
@@ -0,0 +1,11 @@
+<?php
+
+use Kirby\Cms\Page;
+
+use Kirby\Database\Database;
+use Kirby\Database\Sql\Sqlite;
+
+// A collection of all users' pages
+class AccountsPage extends Page
+{
+}
