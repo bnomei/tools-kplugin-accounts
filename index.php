91f9759f9cc761656185f4c0aae2fc18b54a6603
apademide <contact@apademide.ch>
2023-06-06 11:07:55 +0200

    config: load models

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+use Kirby\Cms\App as Kirby;
+
+load([
+  'AccountsPage' => 'models/AccountsPage.php',
+  'AccountPage'  => 'models/AccountPage.php',
+], __DIR__);
+
+Kirby::plugin("auaust/accounts", [
+  'pageModels' => [
+    'accounts' => 'AccountsPage',
+    'account'  => 'AccountPage',
+  ]
+]);
